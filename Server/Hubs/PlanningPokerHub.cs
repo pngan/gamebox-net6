30c6b78ba1d1aa05a644677b9fa2d47bd66870a6
Phillip Ngan <phillip.ngan@gmail.com>
2022-05-07 15:08:56 +1200

    Adding some planning poker hub components - hot reload works

---
+++ Server/Hubs/PlanningPokerHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace gamebox.Server.Hubs;
+
+public class PlanningPokerHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,19 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using gamebox.Server.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+builder.Services.AddSignalR();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
 
 var app = builder.Build();
+app.UseResponseCompression();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -22,15 +26,14 @@ else
 }
 
 app.UseHttpsRedirection();
-
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-
 app.MapRazorPages();
 app.MapControllers();
+app.MapHub<PlanningPokerHub>("/planningpokerhub");
 app.MapFallbackToFile("index.html");
 
 app.Run();
