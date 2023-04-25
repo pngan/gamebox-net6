29564ea57f630ce09f73e3e6c09ca64994f4a2bf
Phillip Ngan <phillip.ngan@gmail.com>
2023-04-25 18:17:36 +1200

    Add the planning poker main play page
    - User can click start game button to navigate to play page
    - All players will navigate to play page when game is started

--- Server/Hubs/PlanningPokerHub.cs
+++ Server/Hubs/PlanningPokerHub.cs
@@ -10,11 +10,17 @@ public class GameHub : Hub
         await Clients.Group(gameCode).SendAsync("ReceiveGameInfo", gameCode, userName);
     }
 
+    public async Task StartGame(string gameCode)
+    {
+        await Clients.Group(gameCode).SendAsync("StartGame", gameCode);
+    }
+
     public async Task JoinGame(string gameCode, string userName)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, gameCode);
     }
 
+    // Don't use this method, because it sends to all clients. Exists as example only. Won't be scalable in real life.
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
